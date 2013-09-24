90ad830c555f1b69f7f0832c78f14f140b359d6b
claaswilke@googlemail.com <claaswilke@googlemail.com@ee17d418-a933-e263-fc85-bbca06d8ca6f>
2013-09-24 07:11:20 +0000

    Hello world example with download image task.

--- workbench/examples/android/helloworld/src/org/jouleunit/helloworld/MainActivity.java
+++ workbench/examples/android/helloworld/src/org/jouleunit/helloworld/MainActivity.java
@@ -1,8 +1,15 @@
 package org.jouleunit.helloworld;
 
-import android.os.Bundle;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		new DownloadImageTask((ImageView) findViewById(R.id.imageView1))
+				.execute("http://www.claaswilke.de/testdata/large.jpg");
 	}
 
 	@Override
@@ -19,4 +29,30 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+		ImageView bmImage;
+
+		public DownloadImageTask(ImageView bmImage) {
+			this.bmImage = bmImage;
+		}
+
+		protected Bitmap doInBackground(String... urls) {
+			
+			String urldisplay = urls[0];
+			Bitmap mIcon11 = null;
+			try {
+				Thread.sleep(5000);
+				InputStream in = new java.net.URL(urldisplay).openStream();
+				mIcon11 = BitmapFactory.decodeStream(in);
+			} catch (Exception e) {
+				Log.e("Error", e.getMessage());
+				e.printStackTrace();
+			}
+			return mIcon11;
+		}
+
+		protected void onPostExecute(Bitmap result) {
+			bmImage.setImageBitmap(result);
+		}
+	}
 }
--- workbench/examples/android/helloworldtest/src/org/jouleunit/helloworld/test/HelloWorldTests.java
+++ workbench/examples/android/helloworldtest/src/org/jouleunit/helloworld/test/HelloWorldTests.java
@@ -4,6 +4,8 @@ import org.jouleunit.helloworld.MainActivity;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.jayway.android.robotium.solo.Solo;
+
 /**
  * Contains test cases to profile the {@link com.fsck.k9.activity.Accounts}
  * activity w.r.t. energy consumption.
@@ -19,7 +21,20 @@ public class HelloWorldTests extends ActivityInstrumentationTestCase2 {
 	/** Constructor. */
 	@SuppressWarnings("unchecked")
 	public HelloWorldTests() {
-		super("org.qualitune.jouleunit.android.helloworld", MainActivity.class);
+		super("org.jouleunit.helloworld", MainActivity.class);
+	}
+
+	/** {@link Solo} object for testing. */
+	private Solo solo;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.test.ActivityInstrumentationTestCase2#setUp()
+	 */
+	@Override
+	public void setUp() {
+		solo = new Solo(getInstrumentation(), getActivity());
 	}
 
 	/**
